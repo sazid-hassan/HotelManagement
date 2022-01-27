3a43fd435145e7d956f5559f4a283de7d8bd1141
sazid-hassan <sazid.khan.hassan@gmail.com>
2022-01-27 14:23:34 +0600

    Final Commit

--- client/src/Pages/home/RoomCard.js
+++ client/src/Pages/home/RoomCard.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 
 import { useHistory } from 'react-router-dom';
 
-import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
@@ -16,55 +15,50 @@ export default function RoomCard(props) {
 
         <Box
             sx={{
-                // boxShadow: 5,
+                boxShadow: 5,
                 m: 2,
-                cursor: "pointer"
+                cursor: "pointer",
+                maxWidth: 300
             }}
-            // height="120"
-            onClick={() => {
-                history.push(`/details/${props.id}`)
-            }}
-
         >
-            <Card sx={{ maxWidth: 300 }}>
 
-                <CardMedia
-                    component="img"
-                    image={props.img}
-                    alt={props.name}
-                    height="140"
-                />
-                <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
-                    <span style={{ margin: 15, color: "#6e7475" }}>
-                        {props.price}
-                    </span>
+            <CardMedia
+                component="img"
+                image={props.img}
+                alt={props.name}
+                height="140"
+            />
+            <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
+                <span style={{ margin: 15, color: "#6e7475" }}>
+                    {props.price}
+                </span>
+            </Typography>
+            <Typography variant="h5" component="div">
+                <span style={{ margin: 15, color: "#6e7475" }}>{props.name}</span>
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                <span style={{ margin: 15, color: "#6e7475" }}>
+                    Category : {props.type}
+                </span>
+            </Typography>
+            <CardContent>
+                <Typography
+                    variant="body2"
+                    color="text.secondary"
+                >
+                    {/* {props.desc.length > 50 ? `${props.desc.slice(0, 50)}...` : props.desc} */}
+                    {props.desc}
                 </Typography>
-                <Typography variant="h5" component="div">
-                    <span style={{ margin: 15, color: "#6e7475" }}>{props.name}</span>
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    <span style={{ margin: 15, color: "#6e7475" }}>
-                        Category : {props.type}
-                    </span>
-                </Typography>
-                <CardContent>
-                    <Typography
-                        variant="body2"
-                        color="text.secondary"
-                    >
-                        {props.desc.length > 50 ? `${props.desc.slice(0, 50)}...` : props.desc}
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button
-                        size="small"
-                        style={{ color: '#ed114f', fontWeight: "bold" }}
-                        onClick={() => history.push("/confirm")}
-                    >
-                        Book Now!
-                    </Button>
-                </CardActions>
-            </Card>
+            </CardContent>
+            <CardActions>
+                <Button
+                    size="small"
+                    style={{ color: '#ed114f', fontWeight: "bold" }}
+                    onClick={() => history.push("/confirm")}
+                >
+                    Book Now!
+                </Button>
+            </CardActions>
         </Box>
 
     );
